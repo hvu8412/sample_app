0f8bc78a969d69c62b9705fdd85d2b714caf1a5c
hvu8412 <hvu8412@gmail.com>
2021-07-15 15:25:06 +0700

    user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'microposts/create'
+  get 'microposts/destroy'
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
+  get "/logout", to: "sessions#destroy"
   root 'static_pages#home'
   get 'static_pages/help'
   get '/signup', to: 'users#new'
