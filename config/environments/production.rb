9200314b8463c96bc2b07131c48e1a09758dcc72
hvu8412 <hvu8412@gmail.com>
2021-07-22 13:45:17 +0700

    fix mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,14 +49,12 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'sample-app-heroku0.herokuapp.com'
-  config.action_mailer.default_url_options = {host: host}
-  ActionMailer::Base.smtp_settings = {
-   address: 'smtp.sendgrid.net',
-   port: '587',
-   :authentication => :plain,
-   user_name: ENV['SENDGRID_USERNAME'],
-   password: ENV['SENDGRID_PASSWORD'],
-   domain: 'heroku.com',
-   enable_starttls_auto: true
- }
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV["USER_EMAIL"],
+    :password             => ENV["USER_PASSWORD"],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
 end
